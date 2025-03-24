2e9786a5db9c077b420d0d847976cfb1538904ba
Rob Proulx <150395800+raproulx@users.noreply.github.com>
2025-03-24 15:29:28 -0500

    Additional example data visualizations

--- src/00-data-exploration.R
+++ src/00-data-exploration.R
@@ -1,9 +1,11 @@
 source("fun/wetbulb_from_relhumid.R")
 source("fun/station_pressure.R")
+library(tidyverse)
+library(ggplot2)
 
-dat <- readr::read_csv("data/00-carrington-test-data.csv") |>
-  dplyr::rowwise() |>
-  dplyr::mutate(
+dat <- read_csv("data/00-carrington-test-data.csv") |>
+  rowwise() |>
+  mutate(
     deltat_f = temp_avg_f -
       wetbulb_from_relhumid(
         Ftemp = temp_avg_f,
@@ -17,3 +19,62 @@ dat <- readr::read_csv("data/00-carrington-test-data.csv") |>
         output_unit = "F"
       )
   )
+
+# test scatterplot - "whole season"
+subdat_scatter <- dat |>
+  filter(month == 6 & hour_cst >= 600 & hour_cst <= 2100)
+
+ggplot(
+  subdat_scatter,
+  aes(x = temp_avg_f, y = wind_spd_max_mph, color = deltat_f)
+) +
+  geom_point() +
+  facet_wrap(~year)
+
+# test linechart - "one week"
+subdat_linewk <- dat |>
+  filter(month == 6 & day <= 6 & year == 2021) |>
+  mutate(
+    timestamp_cst = str_c(year, month, day, sep = "-") |>
+      str_c(hour_cst / 100, sep = " ") |>
+      ymd_h(tz = "US/Central")
+  )
+
+subdat_lineavg <- dat |>
+  filter(month == 6 & day <= 6 & year < 2021) |>
+  group_by(month, day, hour_cst) |>
+  mutate(wind_spd_max_mph = mean(wind_spd_max_mph)) |>
+  mutate(deltat_f = mean(deltat_f)) |>
+  distinct(month, day, hour_cst, wind_spd_max_mph, deltat_f) |>
+  mutate(
+    year = 2021,
+    timestamp_cst = str_c(2021, month, day, sep = "-") |>
+      str_c(hour_cst / 100, sep = " ") |>
+      ymd_h(tz = "US/Central")
+  )
+
+ggplot(
+  subdat_linewk,
+  aes(
+    x = timestamp_cst,
+    y = wind_spd_max_mph,
+    color = deltat_f
+  )
+) +
+  geom_point() +
+  geom_line() +
+  scale_y_continuous(limits = c(0, 40)) +
+  facet_wrap(~year)
+
+ggplot(
+  subdat_lineavg,
+  aes(
+    x = timestamp_cst,
+    y = wind_spd_max_mph,
+    color = deltat_f
+  )
+) +
+  geom_point() +
+  geom_line() +
+  scale_y_continuous(limits = c(0, 40)) +
+  facet_wrap(~year)

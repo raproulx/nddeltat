927f4098a2f14cd4712bf0a70549d56625386171
Rob Proulx <150395800+raproulx@users.noreply.github.com>
2025-03-26 15:03:23 -0500

    function to parse NWS hourly forecast JSON

---
+++ fun/nws_forecast_parse.R
@@ -0,0 +1,56 @@
+# see: https://www.weather.gov/documentation/services-web-api
+library(tidyverse)
+nws_forecast_parse <- function(
+  named_httr2_response = NULL
+) {
+  nws_obj <- named_httr2_response[[1]] |> resp_body_json()
+  location_elev_m <- nws_obj |> pluck("properties", "elevation", "value")
+  location_id <- names(named_httr2_response)
+
+  if (nws_obj |> pluck("properties", "forecastGenerator") |> is.null()) {
+    return("raw")
+  } else {
+    switch(
+      nws_obj |> pluck("properties", "forecastGenerator"),
+      "BaselineForecastGenerator" = {
+        return("12h")
+      },
+      "HourlyForecastGenerator" = {
+        periods <- tibble(
+          forecast_type = "hourly",
+          period = nws_obj |> pluck("properties", "periods")
+        ) |>
+          hoist(
+            period,
+            startTime = "startTime",
+            endTime = "endTime",
+            isDaytime = "isDaytime",
+            temperature = "temperature",
+            temperatureUnit = "temperatureUnit",
+            temperatureTrend = "temperatureTrend",
+            probabilityOfPrecipitation = "probabilityOfPrecipitation",
+            dewpoint = "dewpoint",
+            relativeHumidity = "relativeHumidity",
+            windSpeed = "windSpeed",
+            windDirection = "windDirection",
+            shortForecast = "shortForecast"
+          ) |>
+          unnest_wider(
+            c(probabilityOfPrecipitation, dewpoint, relativeHumidity),
+            names_sep = "_"
+          ) |>
+          separate(windSpeed, c("windSpeed", "windSpeed_unit")) |>
+          mutate(
+            startTime = ymd_hms(startTime) |> with_tz(),
+            endTime = ymd_hms(endTime) |> with_tz(),
+            windSpeed = as.integer(windSpeed)
+          ) |>
+          select(!period) |>
+          janitor::clean_names()
+      }
+    )
+  }
+
+  tibble(location_id, location_elev_m, periods) |>
+    relocate(forecast_type, .before = location_id)
+}

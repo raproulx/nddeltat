50471f60bb496f65bdbe87eeae7bcdaefe497654
Rob Proulx <150395800+raproulx@users.noreply.github.com>
2025-03-31 16:41:19 -0500

    initial commit

---
+++ src/01-update-ndawn-station-list.R
@@ -0,0 +1,79 @@
+library(tidyverse)
+library(xml2)
+library(rvest)
+
+##FIX - replace this with direct read using rvest
+ndawn_web_map <- read_html("https://ndawn.ndsu.nodak.edu/")
+
+ndawn_stations <- tibble(
+  station_name = read_html(ndawn_web_map) |>
+    html_elements("area") |>
+    html_attr("title"),
+  station_id = read_html(ndawn_web_map) |>
+    html_elements("area") |>
+    html_attr("href") |>
+    str_extract("(?<=\\=).*"),
+  station_url = "https://ndawn.ndsu.nodak.edu" |>
+    str_c(
+      read_html(ndawn_web_map) |>
+        html_elements("area") |>
+        html_attr("href"),
+      sep = ""
+    )
+)
+
+ndawn_details <- ndawn_stations |>
+  pull(station_url) |>
+  map(
+    slowly(
+      \(x)
+        read_html(x) |>
+          html_element("#details div") |>
+          html_table() |>
+          filter(X1 != "Details:") |>
+          pivot_wider(names_from = X1, values_from = X2),
+      rate = rate_delay(0.5)
+    )
+  ) |>
+  bind_rows() |>
+  janitor::clean_names()
+
+ndawn_output <-
+  bind_cols(
+    ndawn_stations |> select(station_name, station_id),
+    ndawn_details |>
+      separate_wider_regex(
+        elevation,
+        c(elevation_ft = ".*", " feet \\(", elevation_m = ".*", " meters\\)")
+      ) |>
+      mutate(
+        across(
+          c(latitude, longitude),
+          ~ str_remove(.x, pattern = "°") |> as.numeric()
+        ),
+        across(
+          starts_with("elevation"),
+          as.integer
+        ),
+        period_of_record = str_remove(period_of_record, " to .*") |> as_date(),
+        timezone = lutz::tz_lookup_coords(
+          lat = latitude,
+          lon = longitude,
+          method = "accurate"
+        )
+      ) |>
+      rename(date_est = period_of_record)
+  ) |>
+  relocate(
+    station_id,
+    station_name,
+    location,
+    timezone,
+    latitude,
+    longitude,
+    elevation_ft,
+    elevation_m,
+    date_est
+  )
+
+write_csv(ndawn_output, "./data/01-NDAWN-stations-list.csv")
